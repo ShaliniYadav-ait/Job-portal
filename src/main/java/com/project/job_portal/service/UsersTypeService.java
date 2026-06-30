24c0469f95c89daa4333ee282000c824ea156283
Shalini Yadav <shaliniyadav.ait@gmail.com>
2026-06-30 14:24:24 +0530

    Create usersTypeService to get all the usersTypes from the database

---
+++ src/main/java/com/project/job_portal/service/UsersTypeService.java
@@ -0,0 +1,21 @@
+package com.project.job_portal.service;
+
+import com.project.job_portal.entity.UsersType;
+import com.project.job_portal.repository.UsersTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+
+    private final UsersTypeRepository usersTypeRepository;
+
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll(){
+      return usersTypeRepository.findAll();
+    }
+}
