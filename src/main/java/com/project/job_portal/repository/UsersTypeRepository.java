109f5023f1fb9eab31d67536798f61b59e7c6570
Shalini Yadav <shaliniyadav.ait@gmail.com>
2026-06-30 14:22:16 +0530

    Create UsersTypeRepository

--- src/main/java/com/project/job_portal/repository/UsersTypeRepository.java
+++ src/main/java/com/project/job_portal/repository/UsersTypeRepository.java
@@ -1,4 +1,9 @@
 package com.project.job_portal.repository;
 
-public interface UsersTypeRepository {
+import com.project.job_portal.entity.UsersType;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+
 }
